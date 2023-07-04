dcf19810fb7cc359cd9413827e8389a8359e5cf7
JoabOwala <owalajoab@gmail.com>
2023-07-04 13:53:16 +0300

    arts routes

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,2 +1,47 @@
 class ArtsController < ApplicationController
+
+    #GET /arts
+    def index
+        arts = Art.all
+        render json: arts
+    end
+
+    #GET /arts/:id
+    def show
+        art = Art.find(params[:id])
+        render json: art
+    end
+
+    #POST /arts
+    def create
+        art = Art.new(art_params)
+        if art.save
+            render json: art, status: :created
+        else
+            render json: { errors: art.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    #PATCH /arts/:id
+    def update
+        art = Art.find(params[:id])
+        if art.update(art_params)
+            render json: art
+        else
+            render json: { errors: art.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    #DELETE /arts/:id
+    def destroy
+        art = Art.find(params[:id])
+        art.destroy
+        head :no_content
+    end
+
+    private
+
+    def art_params
+        params.require(:art).permit(:title, :description, :image_url, :medium, :year, :price, :collector_id, :artist_id)
+    end
 end
